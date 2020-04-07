bc09a5e3659eea34a155b9f295b5ea2dbaa09fa1
Thomas Glatt <tglatt@gmail.com>
2020-04-07 21:13:05 +0200

    refactor(formik): remove useFormik

--- src/components/AddPatient/AddPatientForm.js
+++ src/components/AddPatient/AddPatientForm.js
@@ -1,4 +1,4 @@
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import React from "react";
 import { Box, Flex, Button } from "rebass";
 import { Label, Input } from "@rebass/forms";
@@ -8,55 +8,65 @@ import { patientSchema } from "../../lib/validationSchemas";
 const AddPatientForm = props => {
   const { patient, handleSubmit } = props;
 
-  const formik = useFormik({
-    onSubmit: handleSubmit,
-    validationSchema: patientSchema,
-    initialValues: {
-      lastname: patient ? patient.lastname : "",
-      firstname: patient ? patient.firstname : "",
-      dob: patient ? patient.dob : ""
-    }
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <Box mb={4}>
-        <Label htmlFor="lastname">Nom du patient</Label>
-        <Input
-          value={formik.values.lastname}
-          id="lastname"
-          name="lastname"
-          hasError={formik.errors.lastname}
-          onChange={formik.handleChange}
-        />
-      </Box>
-      <Box mb={4}>
-        <Label htmlFor="firstname">Prénom du patient</Label>
-        <Input
-          value={formik.values.firstname}
-          id="firstname"
-          name="firstname"
-          hasError={formik.errors.firstname}
-          onChange={formik.handleChange}
-        />
-      </Box>
-      <Box mb={4}>
-        <Label htmlFor="dob">Date de naissance</Label>
-        <Input
-          value={formik.values.dob}
-          type="date"
-          id="dob"
-          name="dob"
-          hasError={formik.errors.dob}
-          onChange={formik.handleChange}
-        />
-      </Box>
-      <Flex alignItems="center" justifyContent="flex-end">
-        <Box>
-          <Button type="submit">Enregistrer</Button>
-        </Box>
-      </Flex>
-    </form>
+    <Formik
+      initialValues={{
+        lastname: patient ? patient.lastname : "",
+        firstname: patient ? patient.firstname : "",
+        dob: patient ? patient.dob : ""
+      }}
+      validationSchema={patientSchema}
+      onSubmit={handleSubmit}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box mb={4}>
+            <Label htmlFor="lastname">Nom du patient</Label>
+            <Input
+              value={values.lastname}
+              id="lastname"
+              name="lastname"
+              hasError={errors.lastname}
+              onChange={handleChange}
+            />
+          </Box>
+          <Box mb={4}>
+            <Label htmlFor="firstname">Prénom du patient</Label>
+            <Input
+              value={values.firstname}
+              id="firstname"
+              name="firstname"
+              hasError={errors.firstname}
+              onChange={handleChange}
+            />
+          </Box>
+          <Box mb={4}>
+            <Label htmlFor="dob">Date de naissance</Label>
+            <Input
+              value={values.dob}
+              type="date"
+              id="dob"
+              name="dob"
+              hasError={errors.dob}
+              onChange={handleChange}
+            />
+          </Box>
+          <Flex alignItems="center" justifyContent="flex-end">
+            <Box>
+              <Button type="submit">Enregistrer</Button>
+            </Box>
+          </Flex>
+        </form>
+      )}
+    </Formik>
   );
 };
 
