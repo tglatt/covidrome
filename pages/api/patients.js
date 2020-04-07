57e5db588933bf7b093b72873ab366548666dc4d
Mathieu Agopian <mathieu@agopian.info>
2020-04-08 00:24:52 +0200

    Use a uuid (v4) for the patient IDs

--- pages/api/patients.js
+++ pages/api/patients.js
@@ -1,9 +1,10 @@
 import patients from "./patients.json";
+import { v4 as uuidv4 } from "uuid";
 
 export default (req, res) => {
   if (req.method === "POST") {
     const data = JSON.parse(req.body);
-    const patient = { id: patients.length, ...data };
+    const patient = { id: uuidv4(), ...data };
     patients.push(patient);
     res.status(201).json(patient);
   } else {
