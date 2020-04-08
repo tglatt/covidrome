75d54c3fb8f476ed966898f2d8a6ca35e4103f18
Mathieu Agopian <mathieu@agopian.info>
2020-04-08 12:22:07 +0200

    Use a next/link on a patient item

--- src/components/PatientList/PatientListItem.js
+++ src/components/PatientList/PatientListItem.js
@@ -1,28 +1,23 @@
+import NextLink from "next/link";
 import { Box, Card, Flex, Text } from "rebass";
 import { patientListItem } from "./style";
-import Router from "next/router";
 
 const PatientListItem = ({ patient }) => (
-  <Card
-    p={1}
-    m={1}
-    sx={patientListItem}
-    onClick={() => {
-      Router.push(`/patients/${patient.id}`);
-    }}
-  >
-    <Flex justifyContent="flex-start">
-      <Box width={1 / 3}>
-        <Text>{patient.lastname}</Text>
-      </Box>
-      <Box width={1 / 3}>
-        <Text>{patient.firstname}</Text>
-      </Box>
-      <Box width={1 / 3}>
-        <Text>{patient.dob}</Text>
-      </Box>
-    </Flex>
-  </Card>
+  <NextLink href="/patients/[patient_id]" as={`/patients/${patient.id}`}>
+    <Card p={1} m={1} sx={patientListItem}>
+      <Flex justifyContent="flex-start">
+        <Box width={1 / 3}>
+          <Text>{patient.lastname}</Text>
+        </Box>
+        <Box width={1 / 3}>
+          <Text>{patient.firstname}</Text>
+        </Box>
+        <Box width={1 / 3}>
+          <Text>{patient.dob}</Text>
+        </Box>
+      </Flex>
+    </Card>
+  </NextLink>
 );
 
 export { PatientListItem };
