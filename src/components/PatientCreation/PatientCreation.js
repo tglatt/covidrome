3b4054a0281dd510089be1e6672a884564c84f52
Mathieu Agopian <mathieu@agopian.info>
2020-04-10 00:22:30 +0200

    fix(edit): edit a factor if it's new or if explicitely asked

--- src/components/PatientCreation/PatientCreation.js
+++ src/components/PatientCreation/PatientCreation.js
@@ -9,7 +9,7 @@ const PatientCreation = () => {
     const patient = await createPatient(values);
     setSubmitting(false);
 
-    Router.push(`/patients/${patient.id}?edit=1`);
+    Router.push(`/patients/${patient.id}`);
   };
 
   return (
